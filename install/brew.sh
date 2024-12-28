5c4bb270ccb1fe62b339da49382f9ede35f1eac1
Reez <reezpatel@gmail.com>
2024-12-29 00:21:12 +0530

    add: bash nvm

--- install/brew.sh
+++ install/brew.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# NEED: ""
-# AS_ROOT
-# GROUP common
-
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
--- install/nvm.sh
+++ install/nvm.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+source ~/../utils.sh
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+row="
+export NVM_DIR=\"$([ -z \"${XDG_CONFIG_HOME-}\" ] && printf %s \"${HOME}/.nvm\" || printf %s \"${XDG_CONFIG_HOME}/nvm\")\"
+[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"
+"
+
+config_file=$(get_shell_config_file)
+
+insert_or_update_block "nvm_install" "$content" "$config_file"
