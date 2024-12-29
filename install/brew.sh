18ee71136c44d23511e96ce83b3cba3108e26861
Reez <reezpatel@gmail.com>
2024-12-29 09:26:19 +0530

    fix: util

--- install/brew.sh
+++ install/brew.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &>/dev/null; then
+    echo "Brew is not installed. Installing..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Brew is already installed. Skipping."
+fi
--- install/nvm.sh
+++ install/nvm.sh
@@ -2,13 +2,14 @@
 
 # GROUP: node
 
-source ~/../utils.sh
+source ./../utils.sh
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
-row="
-export NVM_DIR=\"$([ -z \"${XDG_CONFIG_HOME-}\" ] && printf %s \"${HOME}/.nvm\" || printf %s \"${XDG_CONFIG_HOME}/nvm\")\"
-[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"
+content="
+export NVM_DIR=\"\$HOME/.nvm\"
+[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"                   # This loads nvm
+[ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\" # This loads nvm bash_completion
 "
 
 config_file=$(get_shell_config_file)
